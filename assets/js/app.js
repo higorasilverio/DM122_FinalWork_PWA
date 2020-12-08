32cd3c39b6f839bfa3a3d3fd03c9a825992355b7
higorasilverio <higorasilverio@outlook.com>
2020-12-07 21:43:33 -0300

    Finished first class with HTML5 cache API implemented

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
-"use strict";
+'use strict'
 
-if ("serviceWorker" in navigator) {
-  const success = () => console.log("Service Worker: Registered");
-  const failed = () => console.log("Service Worker: Register failed");
-  navigator.serviceWorker.register("sw.js").then(success).catch(failed);
+if ('serviceWorker' in navigator) {
+  const success = () => console.log('[Service Worker] registered');
+  const failed = () => console.log('[Service Worker] registration failed');
+
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(success)
+    .catch(failed);
 }
