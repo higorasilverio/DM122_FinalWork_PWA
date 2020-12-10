1745ba88170b5826f89f2fba4ccfa8572b3dbc48
higorasilverio <higorasilverio@outlook.com>
2020-12-10 20:04:40 -0300

    PWA finished and missing refatoration of offline mode

--- assets/js/htmlService.js
+++ assets/js/htmlService.js
@@ -1,3 +1,5 @@
+const doneCssClass = "done";
+
 export default class HtmlService {
   constructor(kittensService) {
     this.kittensService = kittensService;
@@ -15,7 +17,7 @@ export default class HtmlService {
   }
 
   async addTask(description) {
-    const task = { description, done: false};
+    const task = { description, done: false };
     const taskId = await this.kittensService.save(task);
     task.id = taskId;
     this.addToHtmlList(task);
@@ -24,7 +26,24 @@ export default class HtmlService {
   async listTasks() {
     const tasks = await this.kittensService.getAll();
     console.log(tasks);
-    tasks.forEach(task => this.addToHtmlList(task));
+    tasks.forEach((task) => this.addToHtmlList(task));
+  }
+  
+  async deleteTask(li, taskId) {
+    await this.kittensService.delete(taskId);
+    li.remove();
+  }
+
+  async saveTask(taskId, isDone) {
+    const task = await this.kittensService.get(taskId);
+    task.done = isDone;
+    await this.kittensService.save(task);
+  }
+
+  toggleTask(li, taskId) {
+    li.classList.toggle(doneCssClass);
+    const isDone = li.classList.contains(doneCssClass);
+    this.saveTask(taskId, isDone);
   }
 
   addToHtmlList(task) {
@@ -33,10 +52,15 @@ export default class HtmlService {
     const span = document.createElement("span");
     const button = document.createElement("button");
     li.setAttribute("data-item-id", task.id);
+    li.addEventListener("click", () => this.toggleTask(li, task.id));
     span.textContent = task.description;
     button.textContent = "x";
+    button.addEventListener("click", (event) => {
+      event.stopPropagation();
+      this.deleteTask(li, task.id);
+    });
     if (task.done) {
-      li.classList.add("done");
+      li.classList.add(doneCssClass);
     }
     li.appendChild(span);
     li.appendChild(button);
